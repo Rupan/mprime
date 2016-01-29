9e8d577b90604036bd1699dba427a1e1afe36de2
Michael Mohr <akihana@gmail.com>
2016-01-29 00:40:44 -0500

    Move spec file into SPECS, add a script to download sources

---
+++ download-sources.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p SOURCES
+cd SOURCES
+wget http://www.mersenne.org/ftp_root/gimps/p95v287.linux64.tar.gz
+wget http://www.mersenne.org/ftp_root/gimps/p95v287.linux32.tar.gz
+wget http://www.mersenne.org/ftp_root/gimps/p95v287.source.zip
+cd ..
